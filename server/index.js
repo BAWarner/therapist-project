7627b87f56d625d710de224a6f18de9f673911cf
Brandon Warner <warner.brandon.a@gmail.com>
2020-03-11 00:45:16 -0500

    remove middleware in index.js causing build issues

--- server/index.js
+++ server/index.js
@@ -35,10 +35,6 @@ app.use(
 );
 app.use( express.static( `${__dirname}/../build` ) );
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-});
-
 app.listen( SERVER_PORT, () => console.log('Party on, Wayne!') );
 
 // AUTH
